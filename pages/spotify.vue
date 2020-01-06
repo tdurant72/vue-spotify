76bb08afd67536a2b36f97e16d6e7cfaeae4a068
Tyus Durant <contact@tyusdurant.com>
2020-01-06 09:08:45 -0500

    initial layout

---
+++ pages/spotify.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex flex-col h-screen">
+    <div class="bg-blue-500 flex-1 flex">
+      <div class="sidebar bg-red-500">sidebar</div>
+      <div class="content-area bg-red-300 flex-1">content area</div>
+    </div>
+    <div class="bg-blue-300"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
